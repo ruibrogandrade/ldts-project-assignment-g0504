b04ce63c4a69b51a87d7e929772958e44c8479ad
ruibrogandrade <up202007539@edu.fe.up.pt>
2022-01-09 10:58:47 +0000

    Testing tests

---
+++ src/test/groovy/MenuTest.groovy
@@ -0,0 +1,19 @@
+import spock.lang.Specification
+
+import java.awt.Robot;
+
+class MenuTest extends Specification {
+    def 'test menu change(Play+Enter)'() {
+        setup:
+        Robot robot = new Robot();
+        def calls = 0
+        def filter = new Game();
+        filter.run();
+        robot.keyPress(KeyEvent.ENTER);
+        robot.keyRelease(KeyEvent.ENTER);
+        when:
+        filter.drawLevelMenu();
+        then:
+        calls==1
+    }
+}
